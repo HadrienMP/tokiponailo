64d794911efe2d0c7d2345109bd6d89be10d6fca
hadrienmp <git@hadrienmp.fr>
2021-01-10 21:06:37 +0100

    Start another question module that should allow the translation both from and to toki pona

---
+++ src/Question2.elm
@@ -0,0 +1,57 @@
+module Question2 exposing (..)
+
+import Dictionary exposing (Language(..), Word)
+import Random
+import ValueList
+import WeightedWords
+
+
+type alias Question =
+    { word: Word
+    , type_: Type
+    , toTranslate: String
+    }
+
+type alias Answer = String
+
+
+type Type
+    = TokTokiPona
+    | FromTokiPona
+
+pickQuestion: Word -> Random.Generator (Maybe Question)
+pickQuestion word =
+    pickQuestionType
+    |> Random.andThen (toQuestion word)
+
+toQuestion word type_=
+    pickToTranslate word type_
+    |> Random.map (Maybe.map (Question word type_))
+
+pickToTranslate: Word -> Type -> Random.Generator (Maybe String)
+pickToTranslate word type_ =
+    case type_ of
+        TokTokiPona ->
+            WeightedWords.pickMeaning2 word
+        FromTokiPona ->
+            Random.constant <| Just <| word.tokiPona
+
+
+pickQuestionType: Random.Generator Type
+pickQuestionType = Random.uniform TokTokiPona [FromTokiPona]
+
+isRight: Answer -> Question -> Bool
+isRight answer question =
+    case question.type_ of
+        TokTokiPona ->
+            answer == question.word.tokiPona
+        FromTokiPona ->
+            String.split "," answer
+            |> List.map (\answerWord -> hasMeaning answerWord question.word)
+            |> List.foldr (&&) True
+
+hasMeaning: String -> Word -> Bool
+hasMeaning meaning word =
+    ValueList.get French word.meanings
+    |> Maybe.map (\meanings -> List.member meaning meanings)
+    |> Maybe.withDefault False
